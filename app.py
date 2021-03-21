e17b8d05887940769380aa3ea34c4ce2b40f8c10
Ricardo Gambini <ricardo.gambini18@outlook.com>
2021-03-21 08:20:16 -0500

    Api users

--- app.py
+++ app.py
@@ -1,10 +1,57 @@
-from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
+from flask import Flask,request,jsonify
+from flask_mysqldb import MySQL
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/', methods=['GET'])
-def index():
-  return '<h1>Hola mundo</h1>'
+# MySQL Connection
+app.config['MYSQL_HOST'] = 'b9l7iiupadf8mvxind2z-mysql.services.clever-cloud.com'
+app.config['MYSQL_USER'] = 'uajht2lef1p3tiby'
+app.config['MYSQL_PASSWORD'] = 'zARsW6mMsMhwD6Awvgp3'
+app.config['MYSQL_DB'] = 'b9l7iiupadf8mvxind2z'
+
+mysql = MySQL(app)
+
+# settings
+app.secret_key = 'mysecretkey'
+
+@app.route('/api/users', methods=['POST'])
+def add_user():
+  if(request.method == 'POST'):
+    body = request.json
+    email = body['email']
+    username = body['username']
+    uid = body['id']
+    img = body['img']
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO users (email,username,id,img,last_matches,last_scores) VALUES (%s,%s,%s,%s,%s,%s)", (email,username,uid,img,'',''))
+    mysql.connection.commit()
+    return jsonify(({
+      'id': uid,
+      'username': username,
+      'email': email,
+      'img': img,
+      'last_matches': '',
+      'last_scores': ''
+    }))
+
+@app.route('/api/users/<id>', methods=['GET'])
+def get_user(id):
+  if(request.method == 'GET'):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users WHERE id ={0}".format(id))
+    result = cur.fetchall()
+    field = cur.description()
+    column = []
+    for i in field:
+      column.append(i[0])
+    data_user = {}
+    for row in result:
+      for i in range(len(column)):
+        data_user[column[i]] = row[i]
+    return jsonify(data_user)
 
 if __name__ == '__main__':
     app.run(port = 5000, debug = True)
