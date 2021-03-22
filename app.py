5d08978ff52e6df8c7fecda17a58e06bbe9a2d24
AloisArevalo <aregam343@gmail.com>
2021-03-21 21:47:44 -0500

    backend

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+# Swagger Specifications #
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Buscaminas PvP"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix = SWAGGER_URL)
+# End Swagger Specifications #
+
 # MySQL Connection
 app.config['MYSQL_HOST'] = 'b9l7iiupadf8mvxind2z-mysql.services.clever-cloud.com'
 app.config['MYSQL_USER'] = 'uajht2lef1p3tiby'
@@ -65,6 +78,38 @@ def add_feedback():
     cur.execute("INSERT INTO feedback(name,email,message) VALUES (%s,%s,%s)", (name,email,message))
     mysql.connection.commit()
     return jsonify({'response':True})
+
+@app.route('/api/ranking', methods = ['POST','GET'])
+def manage_rankings():
+  if(request.method == 'POST'):
+    body = request.json
+    uuid = body['id']
+    username = body['username']
+    img = body['img']
+    time = body['time']
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO ranking(id,username,img,time) VALUES (%s,%s,%s,%s)", (uuid,username,img,time))
+    cur.execute("INSERT INTO users(last_scores) VALUES (%s) WHERE users.id = '%s'", (time, id))
+    mysql.connection.commit()
+    return jsonify({
+      'id': uuid,
+      'username': username,
+      'img': img,
+      'time': time
+    })
+  elif(request.method == 'GET'):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM ranking ORDER BY time ASC")
+    results = cur.fetchall()
+    field = cur.description
+    column = []
+    for i in field:
+      column.append(i[0])
+    data_ranking = {}
+    for row in results:
+      for i in range(len(column)):
+        data_ranking[column[i]] = row[i]
+    return jsonify(data_ranking)
 
 if __name__ == '__main__':
     app.run(port = 5000, debug = True)
